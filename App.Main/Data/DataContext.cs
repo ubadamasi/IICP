8bb9315fca46186147d0fa23aae2d81dfafb12f1
Usman Badamasi <mac@Usmans-MacBook-Pro.local>
2019-01-22 22:10:49 +0100

    SetupDB

---
+++ App.Main/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using App.Main.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Main.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
