3b137cd67048689bc2c206bde60c56d893703e58
Usman Badamasi <mac@Usmans-MacBook-Pro.local>
2019-01-24 11:20:33 +0100

    privacyPage update

--- App.Main/Controllers/HomeController.cs
+++ App.Main/Controllers/HomeController.cs
@@ -5,27 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using App.Main.Models;
+using App.Main.Data;
 
 namespace App.Main.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAuthRepository _repo;
+        public HomeController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public async Task<IActionResult> Privacy(Value value)
         {
-            return View();
+            var values = await _repo.GetValues();
+            return View(values);
         }
 
         //public async Task<IActionResult> Login() => View();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // }
     }
 }
